dfa1114eab268f7235942b8082aecba7e9850475
Vlad <Saphire.g8@gmail.com>
2022-10-20 18:54:16 +0300

    the better version

--- Homework 19.10/Task7.cs
+++ Homework 19.10/Task7.cs
@@ -4,17 +4,17 @@ class HelloWorld
 {
     static void Main()
     {
-        Console.WriteLine("Enter the number: ");
-        Double num = Double.Parse(Console.ReadLine());
-        int quantity = 0;
-        double[] coins = {0.25 , 0.10 , 0.05, 0.01 };
-       foreach( var i in coins)
-        {
-       
-            quantity +=  Convert.ToInt32(Math.Floor( num / i));
-            num = Math.Round( num - (Math.Floor((num / i)) * i), 2);
-        }
-       Console.WriteLine(quantity);
-             
-    }
+        
+		Console.WriteLine("Enter the number: ");
+		int num = Convert.ToInt32(100 * Double.Parse(Console.ReadLine()));
+		int quantity = 0;
+		int[] coins = { 25, 10, 5, 1 };
+		foreach (var i in coins)
+		{
+
+			quantity += num / i;
+			num -= (num / i) * i;
+		}
+		Console.WriteLine(quantity);
+	}
 }
